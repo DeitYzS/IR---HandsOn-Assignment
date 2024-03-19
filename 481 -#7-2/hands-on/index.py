e95d243b9d95f192c734b813526d043a80db50b8
deityzs <kongkonghihi@gmail.com>
2024-03-19 14:54:54 +0700

    Update assignments

--- 481 -#7-2/hands-on/index.py
+++ 481 -#7-2/hands-on/index.py
@@ -10,22 +10,21 @@ from preprocess import preprocess
 
 app = Flask(__name__)
 dir = Path(os.path.abspath(''))
-print(dir / 'resource/github_bug_prediction_tfidf_vectorizer.pkl' )
-app.tfidf_vec = pickle.load(open(dir / 'resource/github_bug_prediction_tfidf_vectorizer.pkl', 'rb'))
-app.basic_model = pickle.load(open(dir / 'resource/github_bug_prediction_basic_model.pkl', 'rb'))
+tfidf_path = os.path.join(dir, 'resource', 'github_bug_prediction_tfidf_vectorizer.pkl')
+model_path = os.path.join(dir, 'resource', 'github_bug_prediction_basic_model.pkl')
 app.stopword_set = set(stopwords.words())
 app.stemmer = PorterStemmer()
 
-@app.route('/predict_basic', methods=['GET'])
-def predict_basic():
-    res_obj = {'status': 'success'}
-    argsList = request.args.to_dict(flat=False)
-    title = argsList['title'][0]
-    body = argsList['body'][0]
-    predict = app.basic_model.predict_proba(hstack([app.tfidf_vec.transform([preprocess(title, app.stopword_set, app.stemmer)])]))
-    res_obj['predict_as'] = 'bug' if predict[0][1] > 0.5 else 'not bug'
-    res_obj['bug_prob'] = predict[0][1] 
-    return res_obj
+@app.route('/predict_basic', methods=['GET']) 
+def predict_basic(): 
+    response_object = {'status': 'success'} 
+    argList = request.args.to_dict(flat=False) 
+    title = argList['title'][0] 
+    body = argList['body'][0] 
+    predict = app.basic_model.predict_proba(hstack([app.tfidf_vectorizer.transform([preprocess(title, app.stopword_set, app.stemmer)]), app.tfidf_vectorizer.transform([preprocess(body, app.stopword_set, app.stemmer)])])) 
+    response_object['predict_as'] = 'bug' if predict[0][1] > 0.5 else 'not bug' 
+    response_object['bug_prob'] = predict[0][1] 
+    return response_object
 
 
 if __name__ == '__main__': 
